0c02fe99ee6c50c8afb68a05cca9b1a241bc60e6
Kevin Park <kevin.park1217@gmail.com>
2020-09-15 18:53:29 -0400

    Change base image to Ubuntu 20.04 to match the toolchain build env

--- docker/setup-toolchain.sh
+++ docker/setup-toolchain.sh
@@ -6,11 +6,14 @@ TYPE="riscv32-glibc-nightly"
 
 apt install -y unzip
 
+
+#### Prebuilt Toolchain - kevinpark1217 ####
 curl https://github.com/kevinpark1217/riscv-gnu-toolchain/releases/download/$VERSION/$TYPE.zip \
         -L --progress-bar --output $TYPE.zip
 unzip $TYPE.zip
 tar -xvf riscv.tar.gz
 mv riscv /opt/riscv
+
 
 #### Manual Compilation ####
 # cd riscv64-linux/riscv-gnu-toolchain
