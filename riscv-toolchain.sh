385567355b5efe0e60d56d39b9de7b135b8dfb91
Kevin Park <kevin.park1217@gmail.com>
2020-09-15 20:13:33 -0400

    Script to setup riscv toolchain

---
+++ riscv-toolchain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#### Precompiled Toolchain ####
+VERSION="v20200908"
+TYPE="riscv32-glibc-nightly"
+
+#### Prebuilt Toolchain - kevinpark1217 ####
+curl https://github.com/kevinpark1217/riscv-gnu-toolchain/releases/download/$VERSION/$TYPE.zip \
+        -L --progress-bar --output $TYPE.zip
+unzip $TYPE.zip
+tar -xvf riscv.tar.gz
+rm riscv.tar.gz $TYPE.zip
+
+# Setup symlinks
+PREFIX="riscv32-unknown-linux-gnu-"
+PREFIX_SIZE=${#PREFIX}
+cd riscv/bin
+for FILE in $PREFIX*; do
+    FILE_WO_PREFIX=$(echo $FILE | cut -c$((PREFIX_SIZE+1))-);
+    ln -s $FILE $FILE_WO_PREFIX;
+done
